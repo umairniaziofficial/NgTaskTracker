359e69b9e3751848512aedc31a24b8ad38dfdc51
umairniaziofficial <umairniaziofficial@gmail.com>
2024-12-22 17:55:50 +0500

    fixed Route issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    data: {title: 'Home'},
+    pathMatch: 'full',
+    data: { title: 'Home' },
     loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
   },
   {
-    path:"todos",
-    title:"Todos",
-    data:{title: 'Todos'},
-    loadComponent: ()=> import("./todos/todos.component").then(m=> m.TodosComponent)
-  }
+    path: 'todos',
+    title: 'Todos',
+    data: { title: 'Todos' },
+    loadComponent: () => import('./todos/todos.component').then((m) => m.TodosComponent),
+  },
 ];
